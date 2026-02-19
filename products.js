4ff76a4d95983de53edcb24adf191f054f9599bc
HigorLDK <xdarkotr@gmail.com>
2026-02-19 09:57:03 -0300

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const products = {
     installments: "ou 10x de R$ 259,90",
     description: "Impressora laser profissional",
     images: [
-      "img/"
+      "img/monitor22philips/mon1.png"
       "https://www.brother.com.br/-/media/brother/product-catalog-media/images/2022/08/26/12/54/multifuncional_laser_monocromatica_dcpl5652dn_00.png",
       "https://www.brother.com.br/-/media/brother/product-catalog-media/images/2022/08/26/12/55/multifuncional_laser_monocromatica_dcpl5652dn_01.png"
     ],
